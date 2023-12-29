6f81a83608140605bd7ba1c0c89c0ea153722046
Somesh Pundir <104303129+SomeshPundir@users.noreply.github.com>
2023-12-29 18:53:55 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Pagination from './Components/Pagination/Pagination'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className=' h-screen'>
+      <Pagination/>
+    </div>
+  )
+}
+
+export default App
